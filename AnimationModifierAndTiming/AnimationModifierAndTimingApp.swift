9d6e597525fad5f8a700f055135b3b00dcc24282
githubuiux <uiuxista@gmail.com>
2023-11-08 13:53:21 +0300

    Initial Commit

---
+++ AnimationModifierAndTiming/AnimationModifierAndTimingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationModifierAndTimingApp.swift
+//  AnimationModifierAndTiming
+//
+//  Created by Levent on 8.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationModifierAndTimingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationModifierAndTiming/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationModifierAndTiming
+//
+//  Created by Levent on 8.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
